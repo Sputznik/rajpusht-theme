96209e91cef51fd3c1de19279997f8f26143e22e
sonalrodrigues <sonal.rodrigues05@gmail.com>
2021-11-11 17:49:20 +0530

    second commit

--- single.php
+++ single.php
@@ -5,10 +5,12 @@
       <?php if (have_posts()) : while (have_posts()) : the_post(); global $post;?>
       <article <?php post_class();?>>
         <header class="entry-header"><h1 class="entry-title"><?php the_title();?></h1></header>
-        <p><strong><?php the_author(); echo '&nbsp;|&nbsp;'.get_the_date('M j, Y');?></strong></p>
         <div class="entry-summary"><?php _e( do_shortcode( '[orbit_excerpt]' ) );?></div>
+        <p><strong><?php the_author(); echo '&nbsp;|&nbsp;'.get_the_date('M j, Y');?></strong></p>
         <div class="post-thumbnail"><?php _e( do_shortcode( '[orbit_thumbnail size="full"]' ) );?></div>
         <div class="entry-content"><?php the_content(); ?></div>
+        <div class="under"></div>
+        <div class="tag-headline"><?php echo 'Tagged Under:'?></div>
         <div class="post-tags"><?php the_tags( '', '', '' ); ?></div>
         <?php get_template_part( 'partials/author', 'box');?>
         <?php get_template_part( 'partials/post', 'navigation');?>
