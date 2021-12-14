5d9f814c156a242139018cbe61c037e5ad2f4d33
sonalrodrigues <sonal.rodrigues05@gmail.com>
2021-12-14 15:12:04 +0530

    archive-page-updates

---
+++ Partials/post-common.php
@@ -0,0 +1,25 @@
+<?php
+	global $post;
+	$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' );
+	$img_class = "orbit-thumbnail-bg";
+
+	if( !has_post_thumbnail() || !is_array( $thumbnail ) || !$thumbnail[0] ){
+		$img_class .= " no-thumbnail";
+	}
+?>
+
+<div class='<?php _e( $img_class );?>' style='background-image: url( "<?php _e( $thumbnail[0] );?> ");position: relative;'>
+	<a href='<?php the_permalink();?>' style="position: absolute; top:0;left:0;width:100%;height: 100%;"></a>
+</div>
+<div class="orbit-content">
+	<h1 class='orbit-title'><a href='<?php the_permalink();?>'><?php the_title();?></a></h1>
+	<hr>
+	<p>By <?php the_author();?> on <?php the_time( 'F jS Y' );?></p>
+	<div class='orbit-excerpt'><?php the_excerpt();?></div>
+</div>
+<?php
+  $reported = get_post_meta( get_the_ID(), 'publisher', true );
+?>
+<?php if( $reported ): ?>
+	<p>Reported in <? _e( $reported ); ?></p>
+<?php endif;?>
